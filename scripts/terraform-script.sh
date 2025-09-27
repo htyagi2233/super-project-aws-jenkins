63f16f64fffc2746788334c3e1951ba2b8a0d611
htyagi2233 <164169058+htyagi2233@users.noreply.github.com>
2025-09-27 16:59:46 +0530

    Update terraform-script.sh

--- scripts/terraform-script.sh
+++ scripts/terraform-script.sh
@@ -4,20 +4,20 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinu
 sudo yum -y install terraform
 
 
-# for Ubuntu
-# #!/bin/bash
+#for Ubuntu
+#!/bin/bash
 
-# # Update existing packages
-# sudo apt-get update -y
-# sudo apt-get install -y gnupg software-properties-common curl
+# Update existing packages
+sudo apt-get update -y
+sudo apt-get install -y gnupg software-properties-common curl
 
-# # Add the HashiCorp GPG key
-# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# Add the HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-# # Add the official HashiCorp Linux repository
-# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-#   sudo tee /etc/apt/sources.list.d/hashicorp.list
+# Add the official HashiCorp Linux repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+  sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-# # Update and install Terraform
-# sudo apt-get update -y
-# sudo apt-get install -y terraform
+# Update and install Terraform
+sudo apt-get update -y
+sudo apt-get install -y terraform
