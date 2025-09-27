eb6862cd930d1cbe1e57bb610d9d76d538ba718c
htyagi2233 <164169058+htyagi2233@users.noreply.github.com>
2025-09-27 16:59:25 +0530

    Update terraform-script.sh

--- scripts/terraform-script.sh
+++ scripts/terraform-script.sh
@@ -2,3 +2,22 @@
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+
+# for Ubuntu
+# #!/bin/bash
+
+# # Update existing packages
+# sudo apt-get update -y
+# sudo apt-get install -y gnupg software-properties-common curl
+
+# # Add the HashiCorp GPG key
+# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# # Add the official HashiCorp Linux repository
+# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+#   sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# # Update and install Terraform
+# sudo apt-get update -y
+# sudo apt-get install -y terraform
